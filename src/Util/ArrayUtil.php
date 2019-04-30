00b149cf071c288c36d2c8c89467a642f9e711ed
nagatax <4foview@gmail.com>
2019-04-30 20:47:15 +0900

    返却型とPHPDOCの修正
    返却型とPHPDOCを修正する。

--- src/Util/ArrayUtil.php
+++ src/Util/ArrayUtil.php
@@ -7,11 +7,11 @@ class ArrayUtil
     /**
      * arr1がarr2に含まれているか、判定する
      *
-     * @param array $arr1
-     * @param array $arr2
-     * @return bool
+     * @param array $arr1 左オペランド
+     * @param array $arr2 右オペランド
+     * @return bool 比較結果
      */
-    public function isArrayContains(array $arr1, array $arr2)
+    public function isArrayContains(array $arr1, array $arr2): bool
     {
         return array_intersect($arr1, $arr2) ? true : false;
     }
