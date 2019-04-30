820abd759ac9f45da9d4b06610c0f8fed7f495f7
nagatax <4foview@gmail.com>
2019-04-30 23:28:55 +0900

    Redirectクラスの追加
    リダイレクトを行う関数を追加する。

---
+++ src/Http/Redirect.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PHPLibrary\Http;
+
+require_once __DIR__ . "/../bootstrap.php";
+
+class Redirect
+{
+    /**
+     * コンストラクタ
+     */
+    public function __construct()
+    {
+        ;
+    }
+
+    /**
+     * 301(Moved Permanently)でリダイレクトする
+     * (クローラがリダイレクト先のアドレスを新しいリソースと認識して記録する)
+     *
+     * @param string $url URL
+     * @param bool $rep 同名のヘッダがある場合に置き換えるか
+     */
+    public function redirectMovedPermanently(string $url, $rep = true): void
+    {
+        header('Location: ' . $url, $rep, HTTP_REDIRECT_PERM);
+    }
+
+    /**
+     * 302(Found/Moved Temporarily)でリダイレクトする
+     * (クローラがリダイレクト先のアドレスを記録しない)
+     *
+     * @param string $url URL
+     * @param bool $rep 同名のヘッダがある場合に置き換えるか
+     */
+    public function redirectMovedTemporarily(string $url, $rep = true): void
+    {
+        header('Location: ' . $url, $rep, HTTP_REDIRECT_FOUND);
+    }
+
+    /**
+     * 303(See Other)でリダイレクトする
+     *
+     * @param string $url URL
+     * @param bool $rep 同名のヘッダがある場合に置き換えるか
+     */
+    public function redirectSeeOther(string $url, $rep = true): void
+    {
+        header('Location: ' . $url, $rep, HTTP_REDIRECT_POST);
+    }
+}
