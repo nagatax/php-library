881f2a05bf7a24e49f6af47918c223087a121156
nagatax <4foview@gmail.com>
2019-04-30 17:44:59 +0900

    関数の返却型とPHPDOCの追記
    関数の返却型とPHPDOCを追記する。

--- src/Util/MathUtil.php
+++ src/Util/MathUtil.php
@@ -7,12 +7,12 @@ class MathUtil
     /**
      * 無限循環小数を考慮して、加算計算を行う
      *
-     * @param string $left_operand
-     * @param string $right_operand
-     * @param int $scale
-     * @return string
+     * @param string $left_operand 左オペランド
+     * @param string $right_operand 右オペランド
+     * @param int $scale 小数点以下の桁数
+     * @return string 二つの数の和
      */
-    public function bcadd($left_operand, $right_operand, $scale = 0)
+    public function bcadd($left_operand, $right_operand, $scale = 0): string
     {
         return bcadd($left_operand, $right_operand, $scale);
     }
@@ -20,12 +20,12 @@ class MathUtil
     /**
      * 無限循環小数を考慮して、乗算計算を行う
      *
-     * @param string $left_operand
-     * @param string $right_operand
-     * @param int $scale
-     * @return string
+     * @param string $left_operand 左オペランド
+     * @param string $right_operand 右オペランド
+     * @param int $scale 小数点以下の桁数
+     * @return string 二つの数の積
      */
-    public function bcmul($left_operand, $right_operand, $scale = 0)
+    public function bcmul($left_operand, $right_operand, $scale = 0): string
     {
         return bcmul($left_operand, $right_operand, $scale);
     }
